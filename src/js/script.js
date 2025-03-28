7491f3f2e770c6c7fcb36e899e049ff887df40fc
João Henrique <joaohen.t8@gmail.com>
2025-03-28 14:57:13 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,24 @@
+function addValue() {
+    document.getElementById("result")
+
+    let valorBotao = event.target.innerHTML
+
+    document.getElementById("result").value += valorBotao
+}
+
+function calculator() {
+
+    const resultado = document.getElementById("result").value
+    try {
+        const resultadoFinal = eval(resultado)
+        document.getElementById("result2").value = resultadoFinal
+    } catch (error) {
+        document.getElementById("result").value = 'Erro'
+    }
+}
+
+function limpar() {
+    document.getElementById("result").value = ''
+
+}
+
